9f421df251be48d99639677fc705ddd5b9883231
LuisAntonimo <luisantoniossouza@yahoo.com.br>
2020-01-17 23:15:55 -0300

    Criando a base da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://LuisAntonio:Luis35356819@servidor-4j5nn.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -15,6 +20,6 @@ app.use(cors());
 app.use(express.json()); //para o express entender requisições em Json 
 app.use(routes); // para o "app" acessar as rotas
 
-app.listen(3333);
+server.listen(3333);
 
 // Com a função express, o "app" pode fazer uma requisição e acessar a rota "localhost:3333"
